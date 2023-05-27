e8e522dab66d2b31d29f3473e0aa1c68f5a221e2
ApurvaJaiswal3398 <apurvajaiswal123@gmail.com>
2023-05-28 02:52:23 +0530

    Commit 17, Payment Gateway, Validate User Session

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+import braintree
 
 # Create your views here.
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        environment=braintree.Environment.Sandbox,
+        merchant_id='tx9vgt9n3ppjk667',
+        public_key='x7kkympb2nkzfx5s',
+        private_key='8baf48cffcae719fcc97865c0bebdfd2'
+    )
+)
+
+def validate_user_session(id, token):
+    UserModel = get_user_model()
+    try:
+        user = UserModel.objects.get(pk=id)
+        if user.session_token == token:
+            return True
+        return False
+    except UserModel.DoesNotExist:
+        return False
--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, docuemnt_root = settings.MEDIA_ROOT)
